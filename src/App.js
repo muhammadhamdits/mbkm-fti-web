07798a91fef5c86d2fd5647712492dda6b8b67c9
muhammadhamdits <muhammadhamdi702@gmail.com>
2022-10-12 16:43:05 +0700

    add all core components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import DefaultLayout from './layouts/DefaultLayout'
 import HomePage from './pages/Home'
+import LogBookPage from './pages/Logbook'
+import LoginPage from './pages/Login'
 import ProgramDetailPage from './pages/ProgramDetail'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<DefaultLayout />}>
           <Route index element={<HomePage />} />
           <Route path="programs/:id" element={<ProgramDetailPage />} />
+          <Route path="logbooks" element={<LogBookPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
