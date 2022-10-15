547d7753c6d8e721d1b425d3df65e00035afa0d2
Muhammad Hamdi <muhammadhamdi702@gmail.com>
2022-10-15 15:13:24 +0700

    Implement program detail page (#5)
    * Add program detail
    
    * refactor

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,25 +1,36 @@
-import {
-  ListItemButton,
-  ListItemIcon,
-  ListItemText
-} from "@mui/material"
-import { useLocation, useNavigate } from "react-router-dom"
+import * as React from 'react'
+import { List, ListItem, ListItemText, ListItemIcon } from '@mui/material'
+import { Apartment, Assessment, Filter5, WorkspacePremium } from '@mui/icons-material'
 
-const ListItem = ({ icon, path, name }) => {
-  const { pathname } = useLocation()
-  const navigate = useNavigate()
-
+export default function FolderList() {
   return (
-    <ListItemButton
-      selected={ pathname === path }
-      onClick={ () => navigate(path) }
-    >
-      <ListItemIcon>
-        { icon }
-      </ListItemIcon>
-      <ListItemText primary={ name } />
-    </ListItemButton>
+    <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+      <ListItem>
+        <ListItemIcon sx={{ color: 'primary.main' }}>
+          <WorkspacePremium />
+        </ListItemIcon>
+        <ListItemText
+          primary="Bersertifikat"
+          secondary="Kamu akan menerima sertifikat setelah menyelesaikan program ini." />
+      </ListItem>
+      <ListItem>
+        <ListItemIcon sx={{ color: 'primary.main' }}>
+          <Apartment />
+        </ListItemIcon>
+        <ListItemText primary="On-Site" secondary="Kamu akan melaksanakan kegiatan secara on-site." />
+      </ListItem>
+      <ListItem>
+        <ListItemIcon>
+          <Filter5 sx={{ color: 'primary.main' }} />
+        </ListItemIcon>
+        <ListItemText primary="Semester 5" secondary="Setidaknya kamu harus berada pada semester 5 untuk mengikuti program ini." />
+      </ListItem>
+      <ListItem>
+        <ListItemIcon>
+          <Assessment sx={{ color: 'primary.main' }} />
+        </ListItemIcon>
+        <ListItemText primary="20 SKS" secondary="Kamu dapat mengkonversi hingga paling banyak 20 SKS" />
+      </ListItem>
+    </List>
   )
 }
-
-export default ListItem
--- src/pages/Program.jsx
+++ src/pages/Program.jsx
@@ -1,17 +1,107 @@
 import {
-  Paper
+  Paper,
+  Grid,
+  Typography,
+  Button
 } from '@mui/material'
+import ListItem from '../components/ListItem'
 
-const Program = () => {
+const Program = () => {  
   return (
-    <Paper
-      sx={{
-        p: 2,
-        display: 'flex',
-        flexDirection: 'column'
-      }}
-    >
-    </Paper>
+    <Grid container spacing={2}>
+      <Grid item xs={12} md={7} lg={8}>
+        <Paper
+          sx={{
+            p: 2,
+            display: 'flex',
+            flexDirection: 'column'
+          }}
+        >
+          <Typography variant='h5'>
+            Detail Program - Pertukaran Pelajar
+          </Typography>
+
+          <ListItem />
+
+          <Typography variant='subtitle2'>
+            Periode Pendaftaran
+          </Typography>
+          <Typography variant='body2' sx={{ marginBottom: 2 }}>
+            15 Agustus 2022 - 15 September 2022 (1 bulan)
+          </Typography>
+
+          <Typography variant='subtitle2'>
+            Periode Kegiatan
+          </Typography>
+          <Typography variant='body2' sx={{ marginBottom: 2 }}>
+            20 September 2022 - 20 Januari 2023 (4 bulan)
+          </Typography>
+
+          <Typography variant='subtitle2'>
+            Deskripsi Program
+          </Typography>
+          <Typography variant='body2' sx={{ marginBottom: 2 }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae temporibus hic sapiente! Soluta possimus natus nam quas ad! Neque perspiciatis dolorem vitae enim? Laboriosam, recusandae. Quisquam, quod. Quisquam, quod.
+          </Typography>
+
+        </Paper>
+      </Grid>
+      <Grid item xs={12} md={5} lg={4}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column'
+              }}
+            >
+              <Button variant='contained'>
+                Daftar Pada Program Ini
+              </Button>
+              <Typography variant='body2' sx={{ marginTop: 2, alignSelf: 'center' }}>
+                Pendaftaran ditutup dalam: <b>2 Hari lagi</b>
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column'
+              }}
+            >
+              <Typography variant='h6'>
+                Tentang Pertukaran Pelajar
+              </Typography>
+              <Typography variant='body2' sx={{ marginTop: 2 }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, exercitationem doloribus.
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column'
+              }}
+            >
+              <Typography variant='h6'>
+                Tentang Harvard University
+              </Typography>
+              <Typography variant='caption'>
+                Jl. Limau Manis No. 1, Kec. Kebayoran Baru, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12160
+              </Typography>
+              <Typography variant='body2' sx={{ marginTop: 2 }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae temporibus hic sapiente! Soluta possimus natus nam quas ad! Neque perspiciatis dolorem vitae enim? Laboriosam, recusandae. Quisquam, quod. Quisquam, quod.
+              </Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
 
