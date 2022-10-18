cc84e338376dd963227e3780cb7afe6a899f16e1
muhammadhamdits <muhammadhamdi702@gmail.com>
2022-10-18 22:25:31 +0700

    add create agency

--- src/pages/AgencyTable.jsx
+++ src/pages/AgencyTable.jsx
@@ -1,28 +1,21 @@
 import * as React from 'react'
-import PropTypes from 'prop-types'
-import { alpha } from '@mui/material/styles'
 import Box from '@mui/material/Box'
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
-import TablePagination from '@mui/material/TablePagination'
 import TableRow from '@mui/material/TableRow'
-import TableSortLabel from '@mui/material/TableSortLabel'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Paper from '@mui/material/Paper'
-import Checkbox from '@mui/material/Checkbox'
 import IconButton from '@mui/material/IconButton'
 import Tooltip from '@mui/material/Tooltip'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import Switch from '@mui/material/Switch'
-import DeleteIcon from '@mui/icons-material/Delete'
-import FilterListIcon from '@mui/icons-material/FilterList'
-import { visuallyHidden } from '@mui/utils'
-import { Add, AddOutlined, Delete, DeleteOutline, Edit, EditOutlined, RemoveRedEye, Sync } from '@mui/icons-material'
-import { Button, Chip } from '@mui/material'
+import { Add, Delete, Edit } from '@mui/icons-material'
+import Modal from '../components/Modal'
+import axios from 'axios'
+import secureLocalStorage from 'react-secure-storage'
+import { DialogActions, Grid, TextField, Button } from '@mui/material'
 
 const EnhancedTableHead = () => {
   return (
@@ -45,7 +38,9 @@ const EnhancedTableHead = () => {
   )
 }
 
-const EnhancedTableToolbar = () => {
+const EnhancedTableToolbar = (props) => {
+  const setOpen = props.setOpen
+
   return (
     <Toolbar
       sx={{
@@ -62,7 +57,7 @@ const EnhancedTableToolbar = () => {
         Daftar Instansi
       </Typography>
       <Tooltip title="Tampah Program">
-        <IconButton color="primary">
+        <IconButton color="primary" onClick={setOpen}>
           <Add />
           <Typography variant="button" sx={{ ml: 1 }}>
             Tambah
@@ -73,40 +68,151 @@ const EnhancedTableToolbar = () => {
   )
 }
 
-const ProgramTable = () => {
+const AddAgencyForm = () => {
+  const token = secureLocalStorage.getItem('token')
+  const baseURL = process.env.REACT_APP_API_URL
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const params = {
+      name: e.target.name.value,
+      address: e.target.address.value,
+      description: e.target.description.value,
+      webUrl: e.target.webUrl.value,
+      field: e.target.field.value
+    }
+
+    try {
+      const response = await axios.post(`${baseURL}/agencies`, params, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <Box sx={{ width: '100%' }}>
-      <Paper sx={{ width: '100%', mb: 2, paddingX: 2, paddingY: 1 }}>
-        <EnhancedTableToolbar />
-        <TableContainer>
-          <Table
-            sx={{ minWidth: 750 }}
-            aria-labelledby="tableTitle"
-            size="small"
-          >
-            <EnhancedTableHead />
-            <TableBody>
-              <TableRow hover >
-                <TableCell>1</TableCell>
-                <TableCell>Bukalapak</TableCell>
-                <TableCell>
-                  Jl. Limau Manis No. 1, Jakarta Selatan
-                </TableCell>
-                <TableCell sx={{ width: '120px' }}>
-                  <IconButton color='warning'>
-                    <Edit />
-                  </IconButton>
-                  <IconButton color='error'>
-                    <Delete />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Paper>
-    </Box>
+    <form onSubmit={handleSubmit}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            required
+            name="name"
+            label="Nama Instansi"
+            fullWidth
+            variant="standard"
+            margin="normal" />
+          <TextField
+            required
+            name="address"
+            label="Alamat"
+            fullWidth
+            variant="standard"
+            margin="normal" />
+          <TextField
+            name="description"
+            label="Deskripsi"
+            fullWidth
+            multiline
+            maxRows={4}
+            variant="standard"
+            margin="normal" />
+          <TextField
+            required
+            name="webUrl"
+            label="Alamat Website"
+            fullWidth
+            variant="standard"
+            margin="normal" />
+          <TextField
+            name="field"
+            label="Bidang"
+            fullWidth
+            variant="standard"
+            margin="normal" />
+        </Grid>
+      </Grid>
+      <DialogActions>
+        <Button type="submit">
+          Simpan
+        </Button>
+      </DialogActions>
+    </form>
   )
+}
+
+const ProgramTable = () => {
+  const [open, setOpen] = React.useState(false)
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [isLoaded, setIsLoaded] = React.useState(false)
+  const [agencies, setAgencies] = React.useState([])
+  const token = secureLocalStorage.getItem('token')
+  const baseURL = process.env.REACT_APP_API_URL
+
+  const fetchAgencies = async () => {
+    setIsLoading(true)
+    try {
+      const response = await axios.get(`${baseURL}/agencies`, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      setAgencies(response.data.agencies)
+    } catch (error) {
+      console.log(error)
+    }
+    setIsLoading(false)
+    setIsLoaded(true)
+  }
+
+  const handleShowModal = () => {
+    setOpen(!open)
+  }
+
+  React.useEffect(() => {
+    if(!isLoaded && !isLoading) fetchAgencies()
+  }, [isLoaded, isLoading])
+
+  if(isLoading) return <div>Loading...</div>
+  else if(isLoaded && agencies.length){
+    return (
+      <Box sx={{ width: '100%' }}>
+        <Modal
+          open={open}
+          setOpen={handleShowModal}
+          title="Tambah instansi"
+          children={<AddAgencyForm />} />
+        <Paper sx={{ width: '100%', mb: 2, paddingX: 2, paddingY: 1 }}>
+          <EnhancedTableToolbar setOpen={handleShowModal} />
+          <TableContainer>
+            <Table
+              sx={{ minWidth: 750 }}
+              aria-labelledby="tableTitle"
+              size="small"
+            >
+              <EnhancedTableHead />
+              <TableBody>
+                {agencies.map((agency, index) => (
+                  <TableRow hover key={agency.id}>
+                    <TableCell>{index + 1}</TableCell>
+                    <TableCell>{agency.name}</TableCell>
+                    <TableCell>{agency.address}</TableCell>
+                    <TableCell sx={{ width: '120px' }}>
+                      <IconButton color='warning'>
+                        <Edit />
+                      </IconButton>
+                      <IconButton color='error'>
+                        <Delete />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
+      </Box>
+    )
+  } else return <div>Empty</div>
 }
 
 export default ProgramTable
