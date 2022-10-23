a6dd44151fc4631115dd83c3e8dd43f355b5f0a2
muhammadhamdits <muhammadhamdi702@gmail.com>
2022-10-23 21:34:44 +0700

    add set cpmk

--- src/pages/CourseTable.jsx
+++ src/pages/CourseTable.jsx
@@ -1,28 +1,27 @@
 import * as React from 'react'
-import PropTypes from 'prop-types'
-import { alpha } from '@mui/material/styles'
-import Box from '@mui/material/Box'
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableContainer from '@mui/material/TableContainer'
-import TableHead from '@mui/material/TableHead'
-import TablePagination from '@mui/material/TablePagination'
-import TableRow from '@mui/material/TableRow'
-import TableSortLabel from '@mui/material/TableSortLabel'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Paper from '@mui/material/Paper'
-import Checkbox from '@mui/material/Checkbox'
-import IconButton from '@mui/material/IconButton'
-import Tooltip from '@mui/material/Tooltip'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import Switch from '@mui/material/Switch'
-import DeleteIcon from '@mui/icons-material/Delete'
-import FilterListIcon from '@mui/icons-material/FilterList'
-import { visuallyHidden } from '@mui/utils'
-import { AddOutlined, Delete, DeleteOutline, Edit, EditOutlined, RemoveRedEye, Sync } from '@mui/icons-material'
-import { Button, Chip } from '@mui/material'
+import {
+  AddOutlined,
+  Edit,
+  Sync
+} from '@mui/icons-material'
+import {
+  Button,
+  Grid,
+  TextField,
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Toolbar,
+  Typography,
+  Paper,
+  IconButton,
+  Tooltip,
+  DialogActions
+} from '@mui/material'
 import axios from 'axios'
 import secureLocalStorage from 'react-secure-storage'
 import Modal from '../components/Modal'
@@ -77,9 +76,100 @@ const EnhancedTableToolbar = () => {
 }
 
 const CPMKList = (props) => {
-  return (
-    <>Test</>
-  )
+  const { data, baseUrl, token, callback } = props
+  const [cpmks, setCpmks] = React.useState([])
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [isLoaded, setIsLoaded] = React.useState(false)
+
+  const fetchCPMKs = async () => {
+    setIsLoading(true)
+    try {
+      const response = await axios.get(`${baseUrl}/courses/${data.id}/achievements`, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      setCpmks(response.data.achievements)
+    } catch (error) {
+      console.log(error)
+    }
+    setIsLoading(false)
+    setIsLoaded(true)
+  }
+
+  const handleAddInput = (e) => {
+    setCpmks([...cpmks, { achievementCode: '', title: '' }])
+  }
+
+  const handleSaveCPMKs = async (e) => {
+    e.preventDefault()
+    const inputs = document.querySelectorAll('input')
+    const achievementCodes = Array.from(inputs).filter((input) => {
+      if(input.name.includes('achievementCode')) return input.value
+    }).map((input) => input.value)
+    const titles = Array.from(inputs).filter((input) => {
+      if(input.name.includes('title')) return input.value
+    }).map((input) => input.value)
+    try {
+      const payload = {achievementCodes, titles}
+      const response = await axios.put(`${baseUrl}/courses/${data.id}/achievements`,
+        payload,
+        { headers: { Authorization: `Bearer ${token}` } }
+      )
+      callback()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  React.useEffect(() => {
+    if(!isLoaded && !isLoading) fetchCPMKs()
+  }, [isLoaded, isLoading])
+
+  if(isLoading) return <div>Loading...</div>
+  else if(isLoaded){
+    return (
+      <form onSubmit={handleSaveCPMKs}>
+        <Button
+          onClick={handleAddInput}
+          size="small"
+          variant="contained"
+          color="primary"
+          startIcon={<AddOutlined />}
+          sx={{ mb: 2 }} >
+          Tambah CPMK
+        </Button>
+        {cpmks.map((cpmk, index) => (
+          <Grid container spacing={2} key={`${index}-gc`} sx={{ mb: 1 }}>
+            <Grid item xs={3} key={`${index}-gl`}>
+              <TextField
+                required
+                fullWidth
+                label="Kode CPMK"
+                variant="standard"
+                name="achievementCodes[]"
+                defaultValue={cpmk.achievementCode}
+                key={`${index}-kc`} />
+            </Grid>
+            <Grid item xs={9} key={`${index}-gr`}>
+              <TextField
+                required
+                fullWidth
+                label="Nama CPMK"
+                variant="standard"
+                name="titles[]"
+                defaultValue={cpmk.title}
+                key={`${index}-nc`} />
+            </Grid>
+          </Grid>
+        ))}
+        <DialogActions>
+          <Button
+            type="submit">
+            Simpan
+          </Button>
+        </DialogActions>
+      </form>
+    )
+  }
 }
 
 const CourseTable = () => {
@@ -115,6 +205,7 @@ const CourseTable = () => {
   }
 
   const callback = () => {
+    setModalOpen()
   }
 
   React.useEffect(() => {
