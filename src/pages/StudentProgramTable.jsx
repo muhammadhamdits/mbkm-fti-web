048cd945ec14a83124ff6c6e32b46e873d2bcfd3
muhammadhamdits <muhammadhamdi702@gmail.com>
2022-10-23 11:54:44 +0700

    refactor import

--- src/pages/StudentProgramTable.jsx
+++ src/pages/StudentProgramTable.jsx
@@ -1,36 +1,27 @@
 import * as React from 'react'
-import PropTypes from 'prop-types'
-import { alpha } from '@mui/material/styles'
-import Box from '@mui/material/Box'
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableContainer from '@mui/material/TableContainer'
-import TableHead from '@mui/material/TableHead'
-import TablePagination from '@mui/material/TablePagination'
-import TableRow from '@mui/material/TableRow'
-import TableSortLabel from '@mui/material/TableSortLabel'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Paper from '@mui/material/Paper'
-import Checkbox from '@mui/material/Checkbox'
-import IconButton from '@mui/material/IconButton'
-import Tooltip from '@mui/material/Tooltip'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import Switch from '@mui/material/Switch'
-import DeleteIcon from '@mui/icons-material/Delete'
-import FilterListIcon from '@mui/icons-material/FilterList'
-import { visuallyHidden } from '@mui/utils'
-import { Add, AddOutlined, AssignmentLate, AssignmentReturn, AssignmentReturned, AssignmentTurnedIn, Delete, DeleteOutline, Edit, EditOutlined, RemoveRedEye, Sync } from '@mui/icons-material'
+import {
+  AssignmentLate,
+  AssignmentReturned,
+  AssignmentTurnedIn
+} from '@mui/icons-material'
 import {
   Button,
-  Chip,
   DialogActions,
   FormControl,
   InputLabel,
   Select,
   MenuItem,
-  TextField
+  TextField,
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Toolbar,
+  Typography,
+  Paper
 } from '@mui/material'
 import secureLocalStorage from 'react-secure-storage'
 import axios from 'axios'
