6a1ca19d8e91284213606807a7539fa234c2065c
muhammadhamdits <muhammadhamdi702@gmail.com>
2022-10-23 17:55:38 +0700

    init consume from api

--- src/pages/CourseTable.jsx
+++ src/pages/CourseTable.jsx
@@ -23,6 +23,8 @@ import FilterListIcon from '@mui/icons-material/FilterList'
 import { visuallyHidden } from '@mui/utils'
 import { AddOutlined, Delete, DeleteOutline, Edit, EditOutlined, RemoveRedEye, Sync } from '@mui/icons-material'
 import { Button, Chip } from '@mui/material'
+import axios from 'axios'
+import secureLocalStorage from 'react-secure-storage'
 
 const EnhancedTableHead = () => {
   return (
@@ -73,41 +75,70 @@ const EnhancedTableToolbar = () => {
   )
 }
 
-const ProgramTable = () => {
-  return (
-    <Box sx={{ width: '100%' }}>
-      <Paper sx={{ width: '100%', mb: 2, paddingX: 2, paddingY: 1 }}>
-        <EnhancedTableToolbar />
-        <TableContainer>
-          <Table
-            sx={{ minWidth: 750 }}
-            aria-labelledby="tableTitle"
-            size="small"
-          >
-            <EnhancedTableHead />
-            <TableBody>
-              <TableRow hover >
-                <TableCell>1</TableCell>
-                <TableCell>Dasar dasar pemrograman</TableCell>
-                <TableCell>3 SKS</TableCell>
-                <TableCell>
-                  <Button variant="outlined" color="primary" size="small" startIcon={<RemoveRedEye />}>
-                    CPMK
-                  </Button>
-                  <Button variant="outlined" color="primary" size="small" startIcon={<Edit />} sx={{ marginLeft: 1 }}>
-                    Edit
-                  </Button>
-                  <Button variant="outlined" color="error" size="small" startIcon={<Delete />} sx={{ marginLeft: 1 }}>
-                    Delete
-                  </Button>
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Paper>
-    </Box>
-  )
+const CourseTable = () => {
+  const baseUrl = process.env.REACT_APP_API_URL
+  const token = secureLocalStorage.getItem('token')
+  const [courses, setCourses] = React.useState([])
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [isLoaded, setIsLoaded] = React.useState(false)
+
+  const fetchCourses = async () => {
+    setIsLoading(true)
+    try {
+      const response = await axios.get(`${baseUrl}/courses`, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      setCourses(response.data.courses)
+    } catch (error) {
+      console.log(error)
+    }
+    setIsLoading(false)
+    setIsLoaded(true)
+  }
+
+  React.useEffect(() => {
+    if(!isLoaded && !isLoading) fetchCourses()
+  }, [isLoaded, isLoading])
+
+  if(isLoading) return <>Loading</>
+  else if(isLoaded && courses) {
+    return (
+      <Box sx={{ width: '100%' }}>
+        <Paper sx={{ width: '100%', mb: 2, paddingX: 2, paddingY: 1 }}>
+          <EnhancedTableToolbar />
+          <TableContainer>
+            <Table
+              sx={{ minWidth: 750 }}
+              aria-labelledby="tableTitle"
+              size="small"
+            >
+              <EnhancedTableHead />
+              <TableBody>
+                {courses.map((course, index) => (
+                  <TableRow hover key={course.id}>
+                    <TableCell>{index + 1}</TableCell>
+                    <TableCell>{course.name}</TableCell>
+                    <TableCell>{course.sks} SKS</TableCell>
+                    <TableCell>
+                      <Button variant="outlined" color="primary" size="small" startIcon={<RemoveRedEye />}>
+                        CPMK
+                      </Button>
+                      <Button variant="outlined" color="warning" size="small" startIcon={<Edit />} sx={{ marginLeft: 1 }}>
+                        Edit
+                      </Button>
+                      <Button variant="outlined" color="error" size="small" startIcon={<Delete />} sx={{ marginLeft: 1 }}>
+                        Delete
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
+      </Box>
+    )
+  } else  return <>Empty</>
 }
 
-export default ProgramTable
+export default CourseTable
